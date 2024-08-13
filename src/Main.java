b35e3a5cf7371ff331ff91eb90e25c67a4cfa752
20240729 <yoohyeonsik@gmail.com>
2024-08-13 15:33:56 +0900

    책 객체 4개 생성

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import library.Book;
 import library.Library;
 
 public class Main {
     public static void main(String[] args) {
         // 도서관 객체 생성
-        Library library = new Library()
+        Library library = new Library();
+
+        // 책 객체 4개 생성
+        Book bookA = new Book("제목1","작가1",true);
+        Book bookB = new Book("제목2","작가2",true);
+        Book bookC = new Book("제목3","작가3",true);
+        Book bookD = new Book("제목4","작가4",true);
+
+        // 도서관에 책 추가
+
+
+
 
 
 
