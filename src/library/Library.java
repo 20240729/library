3cebadeebb054c0d01679cb35e4adc15cbeb3b63
20240729 <yoohyeonsik@gmail.com>
2024-08-13 15:04:18 +0900

    도서관 책 이름으로 검색까지 함

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,6 +1,34 @@
 package library;
 
+import java.util.ArrayList;
+
 public class Library {
+    // 클래스 속성
+    ArrayList<Book> booklist = new ArrayList<Book>();
 
+    // 생성자(책 목록 초기화 필요) <== 질문할 내용 : 초기화는 바로 위에서 된 것 아닌가요 ㅠㅠ
+    // 잘 모르겠습니다...
+    public Library(ArrayList<Book> books) {
+        this.booklist = books;
+    }
 
+    // 메서드
+    // 도서관에 책 추가
+    public void addBook(Book book) {
+        booklist.add(book);
+    }
+
+    // 도서관에서 책 제거
+    public void removeBook(Book book) {
+        booklist.remove(book);
+    }
+
+    // 도서관에서 책 이름으로 검색
+    public void searchBook(String title) {
+        for (Book book : booklist) {
+            if (book.getTitle().equals(title)) {
+                book.printBookInfo();
+            }
+        }
+    }
 }
