81e5556b43c1de113f770842044ed26125eb030c
20240729 <yoohyeonsik@gmail.com>
2024-08-13 14:40:16 +0900

    실수로 book 내용을 library에 적어서 바꿈

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,44 +1,6 @@
 package library;
 
 public class Library {
-    // 클래스 속성
-    String title = "";
-    String author = "";
-    boolean isAvailable = false;
 
-    // 클래스 생성자
-    public Library(String title, String author, boolean isAvailable) {
-        this.title = title;
-        this.author = author;
-        this.isAvailable = isAvailable;
-    }
-
-    // 메서드
-
-    // 제목 조회
-    public String getTitle() {
-        return title;
-    }
-
-    // 대여 여부 조회
-    public boolean getIsAvailable() {
-        return isAvailable;
-    }
-
-    // 대여 여부 설정
-    public void setIsAvailable(boolean isAvailable) {
-        this.isAvailable = isAvailable;
-    }
-
-    // 책 상세 정보 출력
-    public void printBookInfo(){
-        System.out.println(title);
-        System.out.println(author);
-        if(isAvailable){
-            System.out.println("대여 가능");
-        }else {
-            System.out.println("대여 불가능");
-        }
-    }
 
 }
